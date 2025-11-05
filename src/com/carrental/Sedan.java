adeb41062b5b79d09b85559c25b9926102f534e1
AlexanderR-M <alexanderromano888@gmail.com>
2025-11-05 13:31:54 +0100

    com.carrental

---
+++ src/com/carrental/Sedan.java
@@ -0,0 +1,17 @@
+package com.carrental;
+
+
+
+/** Clase que representa un vehículo tipo Sedan */
+public class Sedan extends Vehiculo {
+    public Sedan(String marca, String modelo, double precioDia) {
+        super(marca, modelo, precioDia);
+    }
+
+    @Override
+    public double calcularCosto(int dias) {
+        return getPrecioDia() * dias;
+    }
+}
+
+
