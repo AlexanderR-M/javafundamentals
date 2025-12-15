db2f9b9866a6f7c337c9ed936a10dbde06bf7f65
AlexanderR-M <alexanderromano888@gmail.com>
2025-12-15 13:10:20 +0100

    file IO

--- src/io/FileOperations.java
+++ src/io/FileOperations.java
@@ -1,12 +1,17 @@
 package io;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
 
 public class FileOperations {
 	public static void main(String[] args) {
-		File file = new File("/logs/user.log");
+		File file = new File("/alex/user.log");
 		
-		File directory = new File("/Alex/logs");
+		File directory = new File("/Alex/logs/");
 		System.out.println("working directory : " + System.getProperty("user.dir"));
 		System.out.println(file.isAbsolute());
 		System.out.println(file.getAbsolutePath());
@@ -14,9 +19,56 @@ public class FileOperations {
 		createPath(directory);
 		createFile(file);
 		
+		createAbsolutePath();
 		//create directory using absolute path
+		
+		writeFile();
+		
+		readFile();
+	}
+
+	private static void readFile()  {
+		// TODO Auto-generated method stub
+		try {
+			FileReader reader = new FileReader("log.txt");
+			System.out.println(reader.read());
+			
+			
+			System.out.println((char)reader.read());
+			System.out.println((char)reader.read());
+			System.out.println((char)reader.read());
+			System.out.println((char)reader.read());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	private static void writeFile() {
+		// TODO Auto-generated method stub
+		try {
+			FileWriter writer = new FileWriter("log.txt");
+			writer.write("Black holes");
+			writer.write("Pablo");
+			writer.write(2323323);
+			//Close the writer and refresh the data to the file
+			writer.close();
+			//refresh data we have written to the file
+			//writer.flush
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+	}
+
+	private static void createAbsolutePath() {
 		File absoluteDir = new File(System.getProperty("user.dir") + "/logs/user.log");
 		absoluteDir.mkdir();
+		
 	}
 
 	private static void createPath(File file) {
